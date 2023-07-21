32cb96e85516acc66c8635d47f480cc9c9c9e7d8
Abhro Kumar Roy <73950310+Abhro631@users.noreply.github.com>
2023-07-21 13:02:19 +0530

    Update console.js

--- console.js
+++ console.js
@@ -11,3 +11,17 @@ function calculateArea() {
 }
 
 calculateArea();
+function calculateArea() {
+  const radius = parseFloat(prompt("Enter the radius of the circle:"));
+
+  if (isNaN(radius) || radius <= 0) {
+    console.log("Please enter a valid positive number for the radius.");
+    return;
+  }
+
+  const area = Math.PI * radius * radius;
+  console.log("The area of the circle is: " + area.toFixed(2));
+}
+
+calculateArea();
+//2nd time
